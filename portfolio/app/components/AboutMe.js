a44a788d4f3efa45fc1e7d961c3bddfe49d99e91
Stephen <sdittawork@gmail.com>
2024-09-16 21:26:53 -0400

    adjusted navbar

--- portfolio/app/components/AboutMe.js
+++ portfolio/app/components/AboutMe.js
@@ -4,9 +4,11 @@ export default function AboutMe() {
 
   return (
     <>
+      <section>
         <div className="w-full h-screen">
-            <h1 className="text-center pt-56 md:text-5xl sm:text-3xl text-xl">About Me</h1>
+          <h1 className="text-center pt-56 md:text-5xl sm:text-3xl text-xl">About Me</h1>
         </div>
+      </section>
     </>
   );
 }
