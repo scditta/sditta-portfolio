8ceb9c508cca39917694977e9744afb8a66948d3
Stephen <sdittawork@gmail.com>
2025-03-07 22:04:00 -0500

    updated firebase config and added apphosting yaml

--- portfolio/firebaseConfig.js
+++ portfolio/firebaseConfig.js
@@ -9,13 +9,17 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.api_key,
-  authDomain: process.env.auth_domain,
-  projectId: process.env.proj_id,
-  storageBucket: process.env.storage_bucket,
-  messagingSenderId: process.env.messaging_sender_id,
-  appId: process.env.app_id,
-  measurementId: process.env.measurement_id,
+  apiKey: process.env.NEXT_PUBLIC_api_key || process.env.API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_auth_domain || process.env.AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_proj_id || process.env.PROJECT_ID,
+  storageBucket:
+    process.env.NEXT_PUBLIC_storage_bucket || process.env.STORAGE_BUCKET,
+  messagingSenderId:
+    process.env.NEXT_PUBLIC_messaging_sender_id ||
+    process.env.MESSAGINS_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_app_id || process.env.APP_ID,
+  measurementId:
+    process.env.NEXT_PUBLIC_measurement_id || process.env.MEASUREMENT_ID,
 };
 
 // Initialize Firebase
