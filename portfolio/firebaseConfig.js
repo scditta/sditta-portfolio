98fa2b8ab2cd3ee0ba13ee93283048fc776c905f
Stephen <sdittawork@gmail.com>
2025-03-23 14:26:44 -0400

    updated analytics: error

--- portfolio/firebaseConfig.js
+++ portfolio/firebaseConfig.js
@@ -20,8 +20,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export const analytics = getAnalytics(app);
